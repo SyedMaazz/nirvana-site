c38fdf806424e76eb792cf43a0996ede821ee7e2
syedmaaz <maazdeys@gmail.com>
2025-12-30 05:18:30 +0530

    made the whole header section

--- src/header.jsx
+++ src/header.jsx
@@ -1,23 +1,42 @@
+import bgVideo from "./assets/videos/nirvana-bg.mp4";
+import nirvanaLogo from "./assets/logo/nirvana_logo.png";
 
 function Header() {
-    return (<>
-      <header>
-        <nav className="nav-bar">
-            <ul>
-                <li className="nav-left">
-                    <span>NIRVANA</span>
-                </li>
-                <li className="nav-right">
-                    <span>MUSIC</span>
-                    <span>VIDEOS</span>
-                    <span>MERCH</span>
-                    <span>MAILING LIST</span>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    </>)
-}
+  return (
+    <header className="header">
+      {/* Background Video */}
+      <video
+        className="video-bg"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src={bgVideo} type="video/mp4" />
+      </video>
 
+      {/* CENTER LOGO */}
+      <div className="center-logo">
+        <img src={nirvanaLogo} alt="Nirvana" />
+      </div>
+
+      {/* NAVBAR */}
+      <nav className="nav-bar">
+        <ul>
+          <li className="nav-left">
+            <img src={nirvanaLogo} alt="Nirvana Logo" className="nav-logo" />
+          </li>
+
+          <li className="nav-right">
+            <span>MUSIC</span>
+            <span>VIDEOS</span>
+            <span>MERCH</span>
+            <span>MAILING LIST</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
--- src/hero.jsx
+++ src/hero.jsx
@@ -1,9 +1,11 @@
-
-function Hero() {   
-    return (
-        <div className="hero">
-        
-        </div>
-    );
+function Hero() {
+  return (
+    <div className="hero">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
+      laudantium accusamus tempora debitis rerum numquam, aperiam beatae minus
+      consequuntur aliquid saepe cum, similique voluptas nulla? Officia culpa
+      illum nisi dolores.
+    </div>
+  );
 }
 export default Hero;
