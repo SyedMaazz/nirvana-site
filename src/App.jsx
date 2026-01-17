1efff135f2b0ae5c2994a4f048605fce4ce25521
syedmaaz <maazdeys@gmail.com>
2026-01-17 17:02:10 +0530

    made footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Discography from './Discography.jsx'
 import Merch from './Merch.jsx'
 import Videos from './Video.jsx'
 import Mailinglist from './Mailinglist.jsx'
-import Footer from './footer.jsx'
+import Footer from './Footer.jsx'
 import Header from './header.jsx'
 
 function App() {
--- src/footer.jsx
+++ src/footer.jsx
@@ -1,9 +1,15 @@
 
-function Footer() {    
-    return (
-        <div className="footer">
-            
-        </div>
-    );
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-content">
+        <span>© 2026 UME</span>
+        <span>TERMS</span>
+        <span>PRIVACY</span>
+        <span className="cookie-choices">/ COOKIE CHOICES</span>
+      </div>
+    </footer>
+  );
 }
+
 export default Footer;
